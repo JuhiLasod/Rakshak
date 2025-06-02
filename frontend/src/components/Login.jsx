717b0bfc550fecfa1d3048d9456ab90f3291ad08
JuhiLasod <juhilasod29@gmail.com>
2025-06-02 15:31:06 +0530

    signin ui done

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,10 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import loginImg from "../components/loginImg.png";
+import logo from "../components/logo.jpg";
+import "../components/Login.css";
 function Login(){
     const navigate=useNavigate();
     const [email,setEmail]=useState("");
     const [password,setPassword]=useState("");
     const [message,setMessage]=useState("");
+    const [loading,setLoading]=useState(false);
 
     useEffect(()=>{
         if(message)
@@ -13,6 +17,7 @@ function Login(){
         }
     },[message]);
     const handleLogin=async()=>{
+        setLoading(true);
         console.log("ending "+email);
         const res=await fetch("http://localhost:8000/api/auth/login",{
             method: "POST",
@@ -25,35 +30,61 @@ function Login(){
         {
             navigate("/home");
         }
+        setLoading(false);
     }
     const handleForget=()=>{
         navigate("/reset-password");
     };
     return (
-        <div>
-            this is login page
-            <div>
-                email
-            </div>
-            <input 
-                type="email"
-                value={email}
-                onChange={(e)=>setEmail(e.target.value)}
-            />
-            <div>
-                password
-            </div>
-            <input 
-                type="password"
-                value={password}
-                onChange={(e)=>setPassword(e.target.value)}
-            />
-            <button onClick={handleLogin}>login</button>
-            <button onClick={handleForget}>forget password</button>
-            <div>
-                {message}
-            </div>
-        </div>
+        
+        <div className="signin2">
+                    <div className="nav2">
+                        <div className="logodiv2"><img className="logo1"src={logo} alt="logo" onClick={()=>navigate("/")}/></div>
+                        <span className="navspan_2 navspan21">Your safety is our priority.</span>
+                        <span className="navspan_2 navspan22">Your digital shield.</span>
+                        <span className="navspan_2 navspan23">where your safety comes first.</span>
+                    </div>
+                    <div className="overall2">
+                    <div className="left2">
+                        <div className="headline2">
+                            <div className="welcome2">
+                            Welcome to <span className="welcomespan2">RAKSHAK </span>! 
+                            </div>
+                            <div className="welcomequote2">
+                            To keep connected , please sign in with your registered email adderess.
+                            </div>
+                        </div>
+                        <div className="inputsdiv2">
+                            <input className="inputs2" type="email"
+                            value={email}
+                            placeholder="@ email address"
+                            onChange={(e)=>{setEmail(e.target.value)}}        
+                            />
+                            <br/>
+                            <br/>
+                            <input className="inputs2" type="password"
+                            value={password}
+                            placeholder="🗝 password"
+                            onChange={(e)=>setPassword(e.target.value)}        
+                            />
+                        </div> 
+                        <div className="fp2" onClick={handleForget}>Forgot password?</div>
+                        <div className="message2">{message}</div>
+                    
+                    <br/>
+                    <button className="signinbtn2" onClick={handleLogin} disabled={loading}>Sign in</button>
+                    <br/>
+                    
+                    <div className="security2">
+                    We guard your data like it's our own — secure, encrypted, and never shared.
+                    </div>
+                    </div>   
+                    
+                        <div className="right2">
+                            <img  className="loginImg2"src={loginImg} alt="hey"/>
+                        </div>
+                    </div>
+                </div>
     )
 };
 export default Login;
