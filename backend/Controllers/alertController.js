cafceaa069cfe36e37ecf472433f8b4cd5f0c5de
JuhiLasod <juhilasod29@gmail.com>
2025-06-06 19:26:15 +0530

    emergency mail sent

---
+++ backend/Controllers/alertController.js
@@ -0,0 +1,52 @@
+import MyPeople from "../Models/MyPeople.js";
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+dotenv.config();
+const alertController=async(req,res)=>{
+    const {email}=req.body;
+    // const pmails=[];
+    const user=await MyPeople.findOne({email});
+    // console.log(user);
+    if (!user || user.people.length === 0) {
+        return res.json("No people found for this email");
+    }
+    try
+    {const pmails = user.people.map(p => p.pmail);
+    const transporter=nodemailer.createTransport({
+        service:"gmail",
+        auth:{
+            user: process.env.MAIL_ID,
+            pass: process.env.PASSWORD
+        }
+    });
+
+    
+        await transporter.sendMail({
+            from: process.env.MAIL_ID,
+            to: pmails,
+            subject: "Emergency Alert from RAKSHAK – Immediate Assistance Required",
+            text: `You are receiving this urgent alert from RAKSHAK regarding a distress signal initiated by ${email}.
+
+This individual is currently facing a critical and potentially dangerous situation and is in immediate need of your assistance. Your prompt response could make a significant difference.
+
+Please proceed without delay to the following location to offer support:
+
+ {link}
+
+Your safety and vigilance are paramount.
+Thank you for standing by those in need.
+
+Warm regards,
+Team RAKSHAK
+Empowering Safety. Enabling Response.`
+        });
+    
+    return res.json("mails sent");
+    }
+    catch(err)
+    {
+        console.log(err);
+        return res.json("couldnt send mails");
+    }
+};
+export default alertController;
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 function Home(){
+    const email=localStorage.getItem("email");
     const navigate=useNavigate();
     const handlePeopleList=()=>{
         navigate("/my-people");
     }
+    const handleAlert=async()=>{
+        const res=await fetch("http://localhost:8000/api/alert/send-alert",{
+            method:"POST",
+            headers: {"Content-Type":"application/json"},
+            body: JSON.stringify({email})
+        });
+        const text=await res.json();
+        window.alert(text);
+    }
     return (
         <div>
             <button onClick={handlePeopleList}>My people</button>
+            <button onClick={handleAlert}>i need help</button>
         </div>
     )
 }
