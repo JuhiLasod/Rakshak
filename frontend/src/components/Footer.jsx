715e0baa1cf37de92fc4591ba9346c36ad4d989c
JuhiLasod <juhilasod29@gmail.com>
2025-06-08 14:32:38 +0530

    footer done

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -10,11 +10,34 @@ function Footer(){
             <div className="footerud">
                 <div className="divforbg"></div>
                 <img className="woodenbg" src={woodenbg} alt="woodenbg"/>
-                <div className="rakshak_btn" onClick={()=>navigate("/login")}>RAKSHAK</div>
+                <div className="rakshak_btn" onClick={()=>window.scrollTo({top: 0, behavior: 'smooth' })}>RAKSHAK</div>
                 <div className="login3"> Sign in</div>
                 <div className="signup3"> Sign up</div>
-                <div className="developer">This website id developed by juhi lasod</div>
-                <div className="copyright">copyright</div>
+                
+                <div className="menu3">
+                    <div className="inner3">Guide me</div> 
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+                    <div className="inner3" onClick={()=>navigate("/about-us")}>About us</div>
+                     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+                     <div className="inner3">Rate us</div> 
+                     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+                     <div className="inner3">Contact us</div> 
+                     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+                     <div className="inner3">Donate</div>
+                     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+                     <div className="inner3">Future goals</div>
+                 </div>
+
+                 <div className="menunext3">
+                    <div className="innernext3">Terms of use</div> 
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+                    <div className="innernext3" >Privacy policy</div>
+                     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+                     <div className="innernext3">SiteMap</div> 
+                     
+                 </div>
+                <div className="developer">This website id developed by Juhi Lasod</div>
+                <div className="copyright">copyright © 2025 Rakshak . All rights reserved.</div>
             </div>
             
         </div>
