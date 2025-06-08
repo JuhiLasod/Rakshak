09e87555e38e8e41cda05a6c370eae5ad1c8ad22
JuhiLasod <juhilasod29@gmail.com>
2025-06-08 12:37:49 +0530

    initial footer part done

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import woodenbg from "./woodenbg.png";
 import "./Footer.css";
+import { useNavigate } from "react-router-dom";
+// import logo from "../components/logo.jpg";
 function Footer(){
+    const navigate=useNavigate();
     return (
         <div>
-            <img className="woodenbg" src={woodenbg} alt="woodenbg"/>
+            <div className="footerud">
+                <div className="divforbg"></div>
+                <img className="woodenbg" src={woodenbg} alt="woodenbg"/>
+                <div className="rakshak_btn" onClick={()=>navigate("/login")}>RAKSHAK</div>
+                <div className="login3"> Sign in</div>
+                <div className="signup3"> Sign up</div>
+                <div className="developer">This website id developed by juhi lasod</div>
+                <div className="copyright">copyright</div>
+            </div>
+            
         </div>
     )
 }
